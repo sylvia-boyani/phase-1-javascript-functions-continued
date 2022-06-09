2416b85f15eb7a46b472f1cf96d36332ef24280a
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-06-09 12:47:21 +0300

    commit tree d91f273dd4e8756584968321c38c90f11bd8f7a0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// // code your solution here
+
+const saturdayFun = (activity = "roller-skate") => {
+    return `This Saturday, I want to ${activity}!`;
+  };
+  const mondayWork = (activity = "go to the office") => {
+    return `This Monday, I will ${activity}.`;
+  };
+  const wrapAdjective = (wrapper) => {
+    return wrapper === "*"
+      ? (adjective) => `You are ${wrapper}${adjective}${wrapper}!`
+      : (adjective) => `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+  
